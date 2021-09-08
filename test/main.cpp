ea01dd5c0989544576f418a5d71d2d20e660f005
Florian Frank <Florian.Frank@uni-passau.de>
2021-09-08 10:22:08 +0200

    Add test execution as console command. Check errors when connecting to a device. Improve code quality.

--- test/main.cpp
+++ test/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <unistd.h>
-#include "pugixml.hpp"
-#include "kst3000.h"
+#include <kst3000.h>
 #include "kst33500.h"
+#include "pugixml.hpp"
+#include "command_line_interface.h"
 
-//using namespace std;
 using namespace pugi;
 
-#define DATA_PATH "/home/florianfrank/Desktop/Measurements"
+#define DATA_PATH "/home/florianfrank/Desktop/Measurements/"
 
 // the max voltage of the chip can be input, unit /10v, here is 1.2V
 int MAX_VOLTAGE = 10;
@@ -29,15 +29,17 @@ void sleep(int secs) {
 
 KST33500 connect_wave_generator() {
   const char *wave_generator_ip = "132.231.14.245";
-  KST33500 wg = KST33500(wave_generator_ip);
-  wg.connect();
+  KST33500 wg(wave_generator_ip);
+  if(!wg.connect())
+      std::cout << "Error while connecting to the wave_generator (" << wg.return_error_message() << ")" <<std::endl;
   return wg;
 }
 
 KST3000 connect_oscilloscope() {
   const char *ip = "132.231.14.243";
-  KST3000 k = KST3000(ip);
-  k.connect();
+  KST3000 k(ip);
+    if(!k.connect())
+        std::cout << "Error while connecting to the oscilloscope (" << k.return_error_message() << ")" <<std::endl;
   return k;
 }
 
@@ -45,7 +47,7 @@ KST3000 connect_oscilloscope() {
  * @brief set params of oscilloscope & waveform generator
  * because every cell has different device parameter to get a better "shape"
  * */
-int set_params(KST33500 wg, KST3000 o, int frequency, int voltage) {
+int set_params(KST33500 &wg, KST3000 &o, int frequency, int voltage) {
   double d_voltage = voltage / 10.0;
   wg.voltage(d_voltage);
   wg.frequency(frequency);
@@ -83,13 +85,13 @@ int set_params(KST33500 wg, KST3000 o, int frequency, int voltage) {
 /**
  * @brief save test meta data
  * */
-int save_meta(string &file_name, int frequency, double voltage, int cell, int chip = 1) {
+int save_meta(std::string &file_name, int frequency, double voltage, int cell, int chip = 1) {
   xml_document doc;
-  string meta_file = std::string(DATA_PATH) + "meta.xml";
+  std::string meta_file = std::string(DATA_PATH) + "meta.xml";
   xml_parse_result result = doc.load_file(meta_file.c_str());
   if(result.status != status_ok)
   {
-      std::cout << "Error while loading document (" << result.description() << ")" << std::endl;
+      std::cout << "Error while loading document (" << meta_file <<  ") Error: (" << result.description() << ")" << std::endl;
       return -1;
   }
   xml_node tests = doc.child("tests");
@@ -97,7 +99,7 @@ int save_meta(string &file_name, int frequency, double voltage, int cell, int ch
     tests = doc.append_child("tests");
   }
 
-  string dup_xpath = "//test[contains(file,'" + file_name + "')]";
+  std::string dup_xpath = "//test[contains(file,'" + file_name + "')]";
   xpath_node dup_test = doc.select_node(dup_xpath.c_str());
   if (dup_test) {
     dup_test.node().parent().remove_child(dup_test.node());
@@ -105,13 +107,13 @@ int save_meta(string &file_name, int frequency, double voltage, int cell, int ch
 
   xml_node test = tests.append_child("test");
   xml_node chip_node = test.append_child("chip");
-  chip_node.append_child(node_pcdata).set_value(to_string(chip).c_str());
+  chip_node.append_child(node_pcdata).set_value(std::to_string(chip).c_str());
   xml_node cell_node = test.append_child("cell");
-  cell_node.append_child(node_pcdata).set_value(to_string(cell).c_str());
+  cell_node.append_child(node_pcdata).set_value(std::to_string(cell).c_str());
   xml_node frequency_node = test.append_child("frequency");
-  frequency_node.append_child(node_pcdata).set_value(to_string(frequency).c_str());
+  frequency_node.append_child(node_pcdata).set_value(std::to_string(frequency).c_str());
   xml_node voltage_node = test.append_child("voltage");
-  voltage_node.append_child(node_pcdata).set_value(to_string(voltage).c_str());
+  voltage_node.append_child(node_pcdata).set_value(std::to_string(voltage).c_str());
   xml_node file_node = test.append_child("file");
   file_node.append_child(node_pcdata).set_value(file_name.c_str());
 
@@ -135,24 +137,24 @@ int save_meta(string &file_name, int frequency, double voltage, int cell, int ch
  *  4, voltage(unit: /10 V);
  *  5, data direction, 1 means input, 2 means output
  * */
-int single_test(int cell, KST33500 wg, KST3000 o, int frequency, int i_voltage) {
+int single_test(int cell, KST33500 &wg, KST3000 &o, int frequency, int i_voltage) {
   double voltage = i_voltage / 10.0;
   wg.voltage(voltage);
   wg.frequency(frequency);
-  string file_prefix = DATA_PATH + to_string(CHIP)
-          + "_" + to_string(cell)
-          + "_" + to_string(frequency)
-          + "_" + to_string(i_voltage);
-  string input_file = file_prefix + "_1";
-  string output_file = file_prefix + "_2";
+  std::string file_prefix = DATA_PATH + std::to_string(CHIP)
+          + "_" + std::to_string(cell)
+          + "_" + std::to_string(frequency)
+          + "_" + std::to_string(i_voltage);
+    std::string input_file = file_prefix + "_1";
+    std::string output_file = file_prefix + "_2";
 
   o.digitize();
   o.set_waveform_source(1);
-  o.save_waveform_data(input_file);
+  o.save_waveform_data(input_file.c_str());
   o.set_waveform_source(2);
-  o.save_waveform_data(output_file);
+  o.save_waveform_data(output_file.c_str());
 
-  std::cout << "Save file in %s\n" << file_prefix << std::endl;
+  std::cout << "Save file in "  << file_prefix << std::endl;
   save_meta(file_prefix, frequency, voltage, cell);
 
   // TODO error code
@@ -168,7 +170,7 @@ int single_test(int cell, KST33500 wg, KST3000 o, int frequency, int i_voltage) 
  *  3, the nth time test, every cell need to be tested many times
  *  4, frequency
  * */
-int test_cell(int cell, const KST33500 &wg, const KST3000 &o) {
+int memristor_frequency_test(int cell, KST33500 &wg, KST3000 &o) {
   for (int f = MIN_FREQUENCY; f <= MAX_FREQUENCY; f += 100) {
     for (int v = MIN_VOLTAGE; v <= MAX_VOLTAGE; v++) {
       set_params(wg, o, f, v);
@@ -182,23 +184,47 @@ int test_cell(int cell, const KST33500 &wg, const KST3000 &o) {
 }
 
 int config(KST3000 &o) {
-  o.set_waveform_points_mode("NORmal");
+  std::string mode = "NORmal";
+  o.set_waveform_points_mode(mode);
   o.set_waveform_points(1000);
 
   // TODO error code
   return 0;
 }
 
+void testCellAllFrequencies(std::string &args)
+{
+    auto argumentList = command_line_interface::splitArguments(args);
+    if(argumentList.empty())
+    {
+        std::cout << "Error: argument list is empty" << std::endl;
+        return;
+    }
+
+    try
+    {
+        int cellID = std::stoi(argumentList[0]);
+
+        KST33500 wg = connect_wave_generator();
+        KST3000 o = connect_oscilloscope();
+        config(o);
+
+        printf("Measure cell %d\n", cellID);
+        memristor_frequency_test(cellID, wg, o);
+    } catch (std::invalid_argument &e)
+    {
+        std::cout << "    Error argument is not a valid cell ID " << std::endl;
+        return;
+    }
+    // TODO check
+}
+
 int main(int argc, char **argv) {
-  if(argc < 2) {
-        std::cout << "Error: CellID must be passed to the program" << std::endl;
-        return 0;
-  }
-  KST33500 wg = connect_wave_generator();
-  KST3000 o = connect_oscilloscope();
-  config(o);
-  int cellID = std::stoi(argv[1]);
-  printf("Measure cell %d\n", cellID);
-  test_cell(cellID, wg, o);
-  return 0;
+
+  command_line_interface cli;
+    command_line_interface::addCustomCommandLineOption("test_all_frequencies", "- Starts a memristor hysterese loop test with different freqnencies",
+                                                       testCellAllFrequencies);
+  cli.start();
+
+    return 0;
 }
