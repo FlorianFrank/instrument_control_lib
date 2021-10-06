e532155881f8842d7e3fd4e3ae08b8e4325413bb
liuwuhao <liuwuhao@zhihu.com>
2021-10-06 11:23:53 +0200

    feat: stablity formal test

--- test/utils.h
+++ test/utils.h
@@ -14,11 +14,19 @@
 #endif //CE_DEVICE_UTILS_H
 
 #ifndef CELL
-#define CELL 1
+#define CELL 16
 #endif
 
 #ifndef APP_TO_FILE
 #define APP_TO_FILE 1
+#endif
+
+#ifndef FREQUENCY
+#define FREQUENCY 100
+#endif
+
+#ifndef THRESHOLD
+#define THRESHOLD 1.2
 #endif
 
 void sleep(double secs);
@@ -36,3 +44,11 @@ int save_meta(string file_name, int frequency, double voltage, int cell, int chi
 int del_meta_tests();
 
 int set_perfect_sin(KST33500 wg);
+
+int measure_resistance(KST33500 wg, KST3000 o, double measure_v);
+
+int set_measurement_pulse(KST33500 wg, double voltage);
+
+int set_conductive_minus_pulse(KST33500 wg);
+
+string get_main_dir();
