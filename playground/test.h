b01a7d84242ba4f5bb7b95297aa09f8580a86b27
wuhao <liuwuhaoo@gmail.com>
2022-06-09 12:36:18 +0200

    add tests to playground

---
+++ playground/test.h
@@ -0,0 +1,18 @@
+//
+// Created by sueaki on 09.06.22.
+//
+
+#ifndef INSTRUMENT_CONTROL_LIB_TEST_H
+#define INSTRUMENT_CONTROL_LIB_TEST_H
+
+#include "iostream"
+#include "KEI2600.h"
+#include "SPD1305.h"
+
+using namespace std;
+
+int testSMU();
+
+int testSPD();
+
+#endif //INSTRUMENT_CONTROL_LIB_TEST_H
