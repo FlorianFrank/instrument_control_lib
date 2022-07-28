e85267175a19aab4ea350674a793aca69c0f34c9
Florian Frank <Florian.Frank@uni-passau.de>
2022-07-28 08:24:56 +0200

    Change general_config to build all libraries (common_tools_lib, pugixml)  static

--- build_config/general_config.cmake
+++ build_config/general_config.cmake
@@ -5,7 +5,7 @@ add_definitions(-DDEVICE_CONFIG_FILE="${CMAKE_CURRENT_SOURCE_DIR}/config_files/S
 #set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -march=x86-64 -static -static-libgcc -static-libstdc++")
 
 
-option(INSTRUMENT_LIB_BUILD_STATIC "Build instrument lib as static library"     OFF)
+option(INSTRUMENT_LIB_BUILD_STATIC "Build instrument lib as static library"     ON)
 
 # Set new CMP0077 policy used to build pugixml
 set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
@@ -18,10 +18,10 @@ add_definitions(-DCXX=1)
 option(PIL_COMMUNICATION    "Enable PIL Sockets"          ON)
 option(PIL_THREADING        "Enable PIL Threads"          ON)
 option(PIL_LOGGING          "Enable Logging support"      ON)
-option(PIL_SHARED           "BUILD PIL as shared library" ON)
-option(PIL_STATIC           "BUILD PIL as static library" OFF)
+option(PIL_SHARED           "BUILD PIL as shared library" OFF)
+option(PIL_STATIC           "BUILD PIL as static library" ON)
 option(PIL_CXX              "Enable PIL C++ support"      ON)
 
 # PugiXML options
-option(BUILD_SHARED_LIBS    "Build pugiXML as shared lib" ON)
+option(BUILD_SHARED_LIBS    "Build pugiXML as shared lib" OFF)
 set(PUGI_XML_VERSION 1.12)
