7bdaed9372b646479a22c78fde54e02d32de2702
Florian Frank <Florian.Frank@uni-passau.de>
2022-09-14 20:51:39 +0200

    Adjust build script on linux, do not build command line tool seperatly

--- build.sh
+++ build.sh
@@ -11,17 +11,3 @@ make
 make install
 popd
 
-
-mkdir build/bin
-pushd build/bin
-cmake ../../memristor_tester -D CMAKE_INSTALL_PREFIX=../../out
-make
-make install
-popd
-
-mkdir build/bin
-pushd build/bin
-cmake ../../instrument_cmd_tool -D CMAKE_INSTALL_PREFIX=../../out
-make
-make install
-popd
