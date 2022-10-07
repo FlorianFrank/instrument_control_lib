b398fb7636a9a2629e6e5e664bb26056b514de31
Alexander Braml <braml11@ads.uni-passau.de>
2022-10-07 10:52:24 +0200

    Pass right IP to testSMU

--- playground/main.cpp
+++ playground/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
     std::cout << "Connect to SMU with IP: " << argv[1] << std::endl;
     // int result = testSPD();
     std::cout << "Start SMU test" << std::endl;
-    int result = testSMU(std::string());
+    int result = testSMU(std::string(argv[1]));
     if (result == 0) {
         std::cout << "Tests PASSED!" << std::endl;
     } else {
