62d83b16d4dc5da5b3a164732b26982bcd34295d
Alexander Braml <braml11@ads.uni-passau.de>
2022-10-07 11:28:53 +0200

    Pass tolerance range to testSMU

--- playground/main.cpp
+++ playground/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
     std::cout << "Connect to SMU with IP: " << argv[1] << std::endl;
     // int result = testSPD();
     std::cout << "Start SMU test" << std::endl;
-    int result = testSMU(std::string(argv[1]));
+    int result = testSMU(std::string(argv[1]), 0.001);
     if (result == 0) {
         std::cout << "Tests PASSED!" << std::endl;
     } else {
