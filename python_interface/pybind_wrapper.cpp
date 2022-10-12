b4d9030c4f65f5292e0af8316e79cb7b05cb8aa1
Alexander Braml <braml11@ads.uni-passau.de>
2022-10-12 13:28:40 +0200

    Add first simple wrapper

--- python_interface/pybind_wrapper.cpp
+++ python_interface/pybind_wrapper.cpp
@@ -1,25 +1,20 @@
 #include "pybind11/pybind11.h"
-
-#include <iostream>
+#include "Device.h"
+#include "devices/types/DCPowerSupply.h"
+#include "devices/SPD1305.h"
+#include "devices/types/SMU.h"
+#include "devices/KEI2600.h"
+#include "devices/KST3000.h"
+#include "devices/KST33500.h"
+#include "ctlib/Logging.hpp"
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
 namespace py = pybind11;
 
-struct Pet {
-    Pet(const std::string &name) : name(name) { }
-    void setName(const std::string &name_) { name = name_; }
-    const std::string &getName() const { return name; }
-    void setNameStd() { name = "Test...Test"; }
-
-    std::string name;
-};
-
 PYBIND11_MODULE(py_instrument_control_lib, m) {
-py::class_<Pet>(m, "Pet")
-.def(py::init<const std::string &>())
-.def("setName", &Pet::setName)
-.def("getName", &Pet::getName)
-.def("setNameStd", &Pet::setNameStd);
+    py::enum_<DCPowerSupply::DC_CHANNEL>(m, "DC_CHANNEL")
+            .value("CHANNEL_1", DCPowerSupply::DC_CHANNEL::CHANNEL_1)
+            .value("CHANNEL_2", DCPowerSupply::DC_CHANNEL::CHANNEL_2);
 }
