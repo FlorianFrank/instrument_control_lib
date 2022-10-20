0adc1300f53ec6047f631e879486fa98a299e505
Florian Frank <Florian.Frank@uni-passau.de>
2022-10-20 20:29:23 +0200

    print out binary dir on github CI

--- build_config/documentation.cmake
+++ build_config/documentation.cmake
@@ -7,13 +7,15 @@ function(build_documentation)
         set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/config_files/doc/Doxyfile.in)
         set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/doc)
 
+        message("BINARY DIR " ${CMAKE_CURRENT_BINARY_DIR})
+
         # request to configure the file
         configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
         message("Doxygen build started")
 
         add_custom_target(generate_documentation ALL
                 COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
-                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/doc
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
                 COMMENT "Generating API documentation with Doxygen"
                 VERBATIM)
 
