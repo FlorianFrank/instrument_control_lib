b624d7eaa08e38cee9eedd5fd1b66d26f0a658ee
Alexander Braml <braml@fim.uni-passau.de>
2022-12-13 11:19:50 +0100

    Add test for measurement buffering

--- playground/main.cpp
+++ playground/main.cpp
@@ -9,10 +9,15 @@ int sweep() {
     auto *smu = new KEI2600(ip.c_str(), 0, nullptr, Device::BUFFER_ENABLED);
     // smu->Connect();
 
+    double value = 0.0;
+
     smu->setLevel(SMU::CURRENT, SMU::CHANNEL_A, 1, false);
     smu->setLevel(SMU::CURRENT, SMU::CHANNEL_A, 2, false);
-    smu->setLevel(SMU::CURRENT, SMU::CHANNEL_A, 3, false);
-    smu->setLevel(SMU::CURRENT, SMU::CHANNEL_A, 4, false);
+
+    smu->measure(SMU::VOLTAGE, SMU::CHANNEL_A, &value, false);
+
+    std::cout << value << std::endl;
+
     std::cout << smu->delay(10) << std::endl;
     std::cout << smu->performLinearVoltageSweep(SMU::SMU_CHANNEL::CHANNEL_A, 0.0, 0.5, 18, 0.01, false) << std::endl;
 
