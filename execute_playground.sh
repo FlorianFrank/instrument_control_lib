f826152aad09e054aa36513cf6b45182dd6e3f98
Alexander Braml <braml@fim.uni-passau.de>
2023-01-13 14:52:24 +0100

    Add script to build and execute playground

--- execute_playground.sh
+++ execute_playground.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-rm -rf out
-rm -rf build
-mkdir build
-mkdir build/lib
-
 pushd build/lib
 cmake ../.. -D CMAKE_INSTALL_PREFIX=../../out 
 make
 make install
 popd
+
+
+./build/lib/playground/playground
