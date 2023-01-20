afeb7bed0af2768118093db24ed8b6ed8a6719d4
Alexander Braml <braml@fim.uni-passau.de>
2023-01-20 15:13:27 +0100

    Add tests in playground

--- playground/main.cpp
+++ playground/main.cpp
@@ -2,9 +2,43 @@
 #include "Device.h"
 #include "devices/KEI2600.h"
 #include "ctlib/Logging.hpp"
-#include <chrono>
 #include <thread>
 
-int main(int argc, char* argv[]) {
+void generic_test() {
+    std::string ip = "132.231.14.168";
+    PIL::Logging logger(PIL::INFO, nullptr);
+    auto *smu = new KEI2600(ip, 0, &logger, Device::SEND_METHOD::DIRECT_SEND);
+
+    smu->Connect();
+
+    smu->setLevel(SMU::VOLTAGE, SMU::CHANNEL_A, 0.42, false);
+
+    smu->disableBeep(false);
+
+    std::cout << smu->GetDeviceIdentifier() << std::endl;
+
+    smu->Disconnect();
+}
+
+void bufferingTest() {
+    std::string ip = "132.231.14.168";
+    PIL::Logging logger(PIL::INFO, nullptr);
+    auto *smu = new KEI2600(ip, 0, &logger, Device::SEND_METHOD::DIRECT_SEND);
+
+    smu->Connect();
+
+    for (int i = 0; i < 10; ++i) {
+        smu->setLevel(SMU::VOLTAGE, SMU::CHANNEL_A, (0.42 + (i / 100.0)), false);
+        smu->delay(i / 5.0);
+    }
+
+    std::cout << smu->executeBufferedScript(false) << std::endl;
+
+    smu->Disconnect();
+}
+
+
+int main() {
+    bufferingTest();
     return 0;
 }
